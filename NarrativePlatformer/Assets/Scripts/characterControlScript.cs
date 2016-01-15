742abf9857970ddc6e803cd67339ec2b143559c8
alexfatemi <videogamesteralex@hotmail.com>
2016-01-15 00:17:24 -0500

    Shader-Player seperation test
    set up a system for the shader to be a seperate trigger object from the
    player controlled via the mouse

---
+++ NarrativePlatformer/Assets/Scripts/characterControlScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterControlScript : MonoBehaviour {
+	public float maxSpeed = 10.0f;
+	private bool facingRight = true;
+	bool grounded = false;
+	public Transform groundCheck;
+	public float groundRadius = 0.2f;
+	private AudioSource jumpSound;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700.0f;
+	Animator anim;
+	private bool movable = false;
+	private Vector3 respawn;
+	// Use this for initialization
+	void Start () {
+		//anim = GetComponent<Animator> ();
+		//jumpSound = this.GetComponent<AudioSource> ();
+		respawn = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		//anim.SetBool ("Ground", grounded);
+		//anim.SetFloat ("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+
+		if (movable){
+			float move = Input.GetAxis ("Horizontal");
+			//anim.SetFloat ("Speed", Mathf.Abs (move));
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			if (move > 0 && !facingRight)
+				Flip ();
+			else if (move < 0 && facingRight)
+				Flip ();
+		}
+	}
+
+	void Update()
+	{
+		if (movable) {
+			if (grounded && Input.GetKeyDown (KeyCode.Space)) {
+				//anim.SetBool ("Ground", false);
+				GetComponent<Rigidbody2D>().AddForce (new Vector2 (0, jumpForce));
+				//jumpSound.Play();
+			}
+		}
+		else {
+			if (Input.GetKeyDown(KeyCode.Space))
+				movable = true;
+		}
+	}
+
+	void Flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
---
+++ NarrativePlatformer/Assets/Scripts/triggerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerController : MonoBehaviour {
+	private bool active = false;
+	public bool sound = false;
+	private AudioSource soundSource;
+	public bool instantiate = false;
+	public Transform spawn;
+	private int timer = 0;
+	public int spawnTime = 40;
+	private Vector3 pos;
+	// Use this for initialization
+	void Start () {
+		if (sound)
+			soundSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sound && active) 
+		    if(!soundSource.isPlaying)
+				soundSource.Play ();
+		if (sound && !active)
+			soundSource.Pause ();
+	}
+	void FixedUpdate () {
+		if (instantiate && active) {
+			timer = timer + 1;
+			if (timer >= spawnTime) {
+				timer = 0;
+				pos = transform.position;
+				pos.z = -3f;
+				Instantiate (spawn, pos, Quaternion.identity);
+			}
+		}
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.layer == LayerMask.NameToLayer("viewSphere")){
+			active = true;
+		}
+	}
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.layer == LayerMask.NameToLayer ("viewSphere")) {
+			active = false;
+		}
+	}
+}
