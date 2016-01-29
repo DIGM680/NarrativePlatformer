61c023b9cb285cf20f56ad19051e617cf6cf3e31
alexfatemi <videogamesteralex@hotmail.com>
2016-01-29 14:54:41 -0500

    Bird Pickup script
    I've set up the bird pickup script and an accompanying example prefab.
    The physics object is 2D while the Trigger that is it's child is 3D like
    the other triggers, so we can keep things separated.

---
+++ NarrativePlatformer/Assets/Scripts/birdPickup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class birdPickup : MonoBehaviour {
+	public GameObject bird;
+	public GameObject physics;
+	public bool click;
+	public bool attached;
+	private Vector3 temPos;
+	// Use this for initialization
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0))
+			click = true;
+		if (Input.GetMouseButtonUp(0)) {
+			click = false;
+			attached = false;
+			temPos = physics.transform.position;
+			temPos.z = 0f;
+			physics.transform.position = temPos;
+		}
+		if (attached && click) {
+			temPos = bird.transform.position;
+			temPos.z = temPos.z-1.74f;
+			//transform.position = temPos;
+			physics.transform.position = temPos;
+			//physics.transform.position = Vector3.MoveTowards(physics.transform.position, temPos, 0.2f);
+		}
+	}
+	void OnTriggerEnter(Collider other) {
+		if ((other.gameObject.layer == LayerMask.NameToLayer("viewSphere")) && click){
+			attached = true;
+		}
+	}
+}
