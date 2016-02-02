ec98be8ad5ec5f6e05716fa2c078f9d0703c6df4
donxu29 <donxu29@gmail.com>
2016-02-02 11:35:05 -0500

    Level 2 progress 2

---
+++ NarrativePlatformer/Assets/Scripts/TriggerRaisePlatofrm.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerRaisePlatofrm : MonoBehaviour {
+    private int triggerCount;
+    public GameObject platform;
+    public float trnaformUp;
+
+    // Use this for initialization
+    void Start () {
+        triggerCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pearl") && triggerCount < 2) {
+            Debug.Log("it's a pearl");
+        }
+        Debug.Log("statue responded");
+    }
+}
