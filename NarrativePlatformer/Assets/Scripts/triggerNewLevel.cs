d5b5dc4911cd259fedd0f811e31bd0305bd309cd
jbrnsk <jbrnsk@gmail.com>
2016-02-12 15:15:25 -0500

    Revert "Revert "Fade added to each level, at start and end. Level end trigger script created, it is currently only in the first level.""
    This reverts commit 6ddc9dfa8349b63c92a7b6e1fdcbe3680225da10.

---
+++ NarrativePlatformer/Assets/Scripts/triggerNewLevel.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerNewLevel : MonoBehaviour {
+
+	private string nextLevelName;
+
+	// Use this for initialization
+	//If we change level names this script needs to be updated. It tells us what level
+	//we are currently in, so we can use this same script to end each level.
+	void Start () {
+	if (Application.loadedLevelName == "level_1") {
+			nextLevelName = "Level2";
+		} else if (Application.loadedLevelName == "Level2") {
+			nextLevelName = "Level3";
+		} else if (Application.loadedLevelName == "level3") {
+			nextLevelName = "level_4";
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D Player){
+		StartCoroutine (NextLevel ());
+	}
+
+	public IEnumerator NextLevel()
+	{
+		float fadeTime = GameObject.Find ("Main Camera").GetComponent<SceneFading> ().BeginFade (1);
+		yield return new WaitForSeconds(fadeTime);
+		Application.LoadLevel (nextLevelName);
+	}
+}
