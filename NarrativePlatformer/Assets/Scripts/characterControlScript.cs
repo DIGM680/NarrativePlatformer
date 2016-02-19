160db579a0a0047e6e7de0a3f1c704dd035656e6
Shihong Zhong <kydist@hotmail.com>
2016-02-19 13:39:45 -0500

    fixed the boy flip problem

--- NarrativePlatformer/Assets/Scripts/characterControlScript.cs
+++ NarrativePlatformer/Assets/Scripts/characterControlScript.cs
@@ -30,10 +30,7 @@ public class characterControlScript : MonoBehaviour {
 			float move = Input.GetAxis ("Horizontal");
 			//anim.SetFloat ("Speed", Mathf.Abs (move));
 			GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
-			if (move > 0 && !facingRight)
-				Flip ();
-			else if (move < 0 && facingRight)
-				Flip ();
+
 		}
 	}
 
@@ -51,11 +48,5 @@ public class characterControlScript : MonoBehaviour {
 				movable = true;
 		}
 	}
-
-	void Flip() {
-		facingRight = !facingRight;
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+		
 }
