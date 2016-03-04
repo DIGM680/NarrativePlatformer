15fb4b6ed07399b273d970208da71fc1543f2d35
jbrnsk <jbrnsk@gmail.com>
2016-03-04 13:55:25 -0500

    Boop?
    Please work

---
+++ NarrativePlatformer/Assets/showTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class showTrigger : MonoBehaviour {
+
+    public GameObject obj;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D Player)
+    {
+        if (Player.gameObject.layer == LayerMask.NameToLayer("character"))
+        {
+            obj.SetActive(true);
+        }
+    }
+}
